0f06af41df4dbb5b8d3bb0ea1da7f87cbae1403e
vgulerianb <90599235+vgulerianb@users.noreply.github.com>
2023-08-27 01:48:27 +0530

    Init landing age

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import "./globals.css";
+import "./global.sass";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
---
+++ app/new/page.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+export default function Home() {
+  return (
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full flex items-center flex-col transition-all">
+        <body translate="no">
+          <div id="stars"></div>
+          <div id="stars2"></div>
+          <div id="stars3"></div>
+          <div id="title">
+            <span>PURE CSS</span>
+            <span>PARALLAX PIXEL STARS</span>
+          </div>
+        </body>
+      </div>
+    </div>
+  );
+}
