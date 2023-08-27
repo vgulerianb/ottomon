58e55a74756d03bc67c52bfd9a1b2b2e508f7cee
vgulerianb <90599235+vgulerianb@users.noreply.github.com>
2023-08-28 00:52:50 +0530

    Ask button fix

--- app/page.tsx
+++ app/page.tsx
@@ -263,7 +263,7 @@ const BotBoddy = () => {
               placeholder="Ask me something"
             />
             <button
-              disabled
+              onClick={!loading ? () => handleSearch(search) : undefined}
               className="bg-[rgb(133,89,244)] text-sm px-[8px] rounded-md"
             >
               Ask
